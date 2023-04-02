eaaaa16c073d39894bb343ace7638a2c5f8031a2
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-03 00:36:22 +0200

    !

---
+++ IWA5/scripts.js
@@ -0,0 +1,46 @@
+let FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = '0';
+
+
+const userShippingcost = () => {
+    if (location === "RSA") { 
+    let userCost = 0
+    let user = 0
+      console.log(shipping === 400 && currency === 'R')
+      if(userCost >= 1000 && user > 1 ) {
+      console.log(shipping = 0)
+     }
+    }
+    if (location = "NK"){
+        console.log(BANNED_WARNING)
+        }
+}
+
+const checkUserCost = () => {
+if (location = "NAM"){
+    let userCost = 0
+    let user = 0
+    console.log(shipping = 600 && currency === '$')
+    if(userCost >= 1000 && user > 1 ) {
+       console.log(shipping = 0)
+    }
+   }
+}
+
+{
+
+
+shoes = 300 * 1;
+toys = 100 * 5;
+batteries = 35 * 2;
+let price = 0;
+const userLocation =  (location === "RSA")
+const calculatePrice = userLocation ? console.log(BANNED_WARNING) : console.log("Price", price = `${shoes + toys + batteries + 400}`)
+
+}
+
+
+
+
+	
