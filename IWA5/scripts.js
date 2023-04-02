3de72860733f22cbc10b6db29a357113c9fce16a
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-03 00:58:05 +0200

    addition to script.js

--- IWA5/scripts.js
+++ IWA5/scripts.js
@@ -1,22 +1,25 @@
+// Define constants for warning messages and none selected option
 let FREE_WARNING = 'Free shipping only applies to single customer orders';
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
 const NONE_SELECTED = '0';
 
-
+// calculating shipping cost based on user's location
 const userShippingcost = () => {
     if (location === "RSA") { 
-    let userCost = 0
+    let userCost = 0 
     let user = 0
-      console.log(shipping === 400 && currency === 'R')
-      if(userCost >= 1000 && user > 1 ) {
-      console.log(shipping = 0)
+      console.log(shipping === 400 && currency === 'R') // Log a message if shipping is 400 and currency is R
+      if(userCost >= 1000 && user > 1 ) { // If userCost is >= 1000 and user is > 1
+      console.log(shipping = 0) // Set shipping to 0
      }
     }
-    if (location = "NK"){
-        console.log(BANNED_WARNING)
+    if (location = "NK"){ 
+        console.log(BANNED_WARNING) // Log a message that we do not ship to this country
+    }
         }
-}
-
+ 
+    
+// Function to check user's cost based on location 
 const checkUserCost = () => {
 if (location = "NAM"){
     let userCost = 0
@@ -30,8 +33,8 @@ if (location = "NAM"){
 
 {
 
-
-shoes = 300 * 1;
+// addition of sums of products purchased
+    shoes = 300 * 1;
 toys = 100 * 5;
 batteries = 35 * 2;
 let price = 0;
@@ -39,6 +42,7 @@ const userLocation =  (location === "RSA")
 const calculatePrice = userLocation ? console.log(BANNED_WARNING) : console.log("Price", price = `${shoes + toys + batteries + 400}`)
 
 }
+
 
 
 
